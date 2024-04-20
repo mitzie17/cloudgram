993b39a0bdc65e2ca76efdde75f1919eec345e2f
Mitzi Estrada <mitziestrada@Mitzis-Air.lan>
2024-04-19 22:27:26 -0700

    create instances for each of appwrite's functionalities

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -3,4 +3,16 @@ import { Client, Account, Databases, Storage, Avatars } from "appwrite";
 
 export const appwriteConfig = {
   projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  url: import.meta.env.VITE_APPWRITE_URL,
 };
+
+// Here an instance of each is created
+export const client = new Client();
+// Below is the client configuration
+client.setProject(appwriteConfig.projectId);
+client.setEndpoint(appwriteConfig.url);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export const avatars = new Avatars(client);
