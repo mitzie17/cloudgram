4cb8762bc4e32c50c614f79af4c48e602f610ddf
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-05-04 21:34:32 -0700

    define functions for creating an account and saving it to the database as well as for signing into an account

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,7 +1,8 @@
 import { ID } from "appwrite";
 import { INewUser } from "@/types";
-import { account } from "./config";
+import { account, appwriteConfig, avatars, databases } from "./config";
 
+// Here, function authenticates user to create account in appwrite, (creates user in appwrite's authentication functionality)
 export async function createUserAccount(user: INewUser) {
   try {
     const newAccount = await account.create(
@@ -10,9 +11,52 @@ export async function createUserAccount(user: INewUser) {
       user.password,
       user.name
     );
-    return newAccount;
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl,
+    });
+
+    return newUser;
   } catch (error) {
     console.log(error);
     return error;
+  }
+}
+
+// Here, user is saved to the actual database
+export async function saveUserToDB(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
+    return newUser;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function signInAccount(user: { email: string; password: string }) {
+  try {
+    const session = await account.createSession(user.email, user.password);
+
+    return session;
+  } catch (error) {
+    console.log(error);
   }
 }
