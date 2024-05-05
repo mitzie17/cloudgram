873f32d60e6935daa8fac32072b0778e1f58026a
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-05-04 21:44:45 -0700

    file contains functions that are called in the app and use as hooks

---
+++ src/lib/react-query/queriesAndMutations.ts
@@ -0,0 +1,24 @@
+// These imports are needed from ReactQuery (aka tanStack Query). useQuery is needed for fetching data, while
+// useMutation is needed for modifying data. ReactQuery (tanStack Query) simplifies data fectching and mutation while
+// allowing to get all the benefits of caching, infinite scrolling, etc.
+import { INewUser } from "@/types";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  useInfiniteQuery,
+} from "@tanstack/react-query";
+import { createUserAccount, signInAccount } from "../appwrite/api";
+
+export const useCreateUserAccount = () => {
+  return useMutation({
+    mutationFn: (user: INewUser) => createUserAccount(user),
+  });
+};
+
+export const useSignInAccount = () => {
+  return useMutation({
+    mutationFn: (user: { email: string; password: string }) =>
+      signInAccount(user),
+  });
+};
