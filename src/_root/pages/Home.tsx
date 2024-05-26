31458a3308152c152a49a973faa1dd84631c62b1
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-05-25 22:17:02 -0700

    create div that will contain all posts and import useGetRecentPosts to fetch all posts

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,5 +1,24 @@
+import Loader from "@/components/shared/Loader";
+import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutations";
+
 const Home = () => {
-  return <div>Home</div>;
+  // For every query, a data object is returned, which can be renamed
+  const {
+    data: posts,
+    isPending: isPostLoading,
+    isError: isErrorPosts,
+  } = useGetRecentPosts();
+
+  return (
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h3-bold md:h2-bold text-left w-full ">Home Feed</h2>
+          {isPostLoading && !posts ? <Loader /> : <ul>Test</ul>}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
