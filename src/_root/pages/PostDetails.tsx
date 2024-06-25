60be9cb041b790de5b13261d4311680d610dcf9c
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-06-24 17:10:06 -0700

    add delete button and div containing post caption and tags

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,5 +1,6 @@
 import Loader from "@/components/shared/Loader";
 import PostCard from "@/components/shared/PostCard";
+import { Button } from "@/components/ui/button";
 import { useUserContext } from "@/context/AuthContext";
 import { useGetPostById } from "@/lib/react-query/queriesAndMutations";
 import { multiFormatDateString } from "@/lib/utils";
@@ -10,6 +11,8 @@ const PostDetails = () => {
   const { id } = useParams();
   const { data: post, isPending } = useGetPostById(id || "");
   const { user } = useUserContext();
+
+  const handleDeletePost = () => {};
 
   return (
     <div className="post_details-container">
@@ -50,7 +53,7 @@ const PostDetails = () => {
                 </div>
               </Link>
 
-              <div className="flex-center gap-4">
+              <div className="flex-center">
                 <Link
                   to={`/update-post/${post?.$id}`}
                   className={`${user.id !== post?.creator.$id && "hidden"}`}
@@ -62,7 +65,34 @@ const PostDetails = () => {
                     alt="edit"
                   />
                 </Link>
+                <Button
+                  onClick={handleDeletePost}
+                  variant="ghost"
+                  className={`ghost_details-delete_btn ${
+                    user.id !== post?.creator.$id && "hidden"
+                  }`}
+                >
+                  <img
+                    src="/assets/icons/delete.svg"
+                    alt="delete"
+                    width={24}
+                    height={24}
+                  />
+                </Button>
               </div>
+            </div>
+
+            <hr className="border w-full border-dark-4/80" />
+
+            <div className="flex flex-col flex-1 w-full small-medium lg:base-regular">
+              <p>{post?.caption}</p>
+              <ul className="flex gap-1 mt-2">
+                {post?.tags.map((tag: string) => (
+                  <li key={tag} className="text-light-3">
+                    #{tag}
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
