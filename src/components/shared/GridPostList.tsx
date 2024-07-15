73d7bd656259cec645337008e3260c886be6bced
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2024-07-15 14:15:21 -0700

    display creator and pass post to PostStats

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -1,12 +1,19 @@
 import { useUserContext } from "@/context/AuthContext";
 import { Models } from "appwrite";
 import { Link } from "react-router-dom";
+import PostStats from "./PostStats";
 
 type GridPostListProps = {
   posts: Models.Document[];
+  showUser?: boolean;
+  showStats?: boolean;
 };
 
-const GridPostList = ({ posts }: GridPostListProps) => {
+const GridPostList = ({
+  posts,
+  showUser = true,
+  showStats = true,
+}: GridPostListProps) => {
   const { user } = useUserContext();
 
   return (
@@ -16,6 +23,20 @@ const GridPostList = ({ posts }: GridPostListProps) => {
           <Link to={`/posts/${post.$id}`} className="grid-post_link">
             <img src={post.imageUrl} alt="post" className="h-full" />
           </Link>
+
+          <div className="grid-post_user">
+            {showUser && (
+              <div className="flex items-center justify-start gap-2 flex-1">
+                <img
+                  src={post.creator.imageUrl}
+                  alt="creator"
+                  className="h-8 w-8 rounded-full"
+                />
+                <p className="line-clamp-1">{post.creator.name}</p>
+              </div>
+            )}
+            {showStats && <PostStats post={post} userId={user.id} />}
+          </div>
         </li>
       ))}
     </ul>
